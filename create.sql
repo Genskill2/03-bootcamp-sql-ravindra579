1aa975239df4979b4b468c1b6767d42606854af5
ravindra <ravindrareddy1217@gmail.com>
2021-06-19 18:03:18 +0530

    fixed

--- create.sql
+++ create.sql
@@ -1,22 +1,21 @@
-CREATE TABLE publisher(
-  id INTEGER PRIMARY KEY,
-  name TEXT,
-  country TEXT
-  );
-  
-  CREATE TABLE books(
-  id INTEGER PRIMARY KEY,
-  title TEXT,
-  publisher INTEGER  REFERENCES publsher(id)
-  );
-  
-  CREATE TABLE subjects(
-  id INTEGER PRIMARY KEY,
-  name TEXT
-  );
-  
-  CREATE TABLE books_subjects(
-  book INTEGER  REFERENCES books(id),
-  subject INTEGER REFERENCES subjects(id)
-  );
-  
+CREATE TABLE publisher (
+	id INTEGER PRIMARY KEY,
+	name TEXT,
+	country TEXT
+);
+CREATE TABLE books (
+	id INTEGER PRIMARY KEY,
+	title TEXT,
+	publisher INTEGER,
+	FOREIGN KEY(publisher) REFERENCES publisher(id)
+);
+CREATE TABLE subjects (
+	id INTEGER PRIMARY KEY,
+	name TEXT);
+	
+CREATE TABLE books_subjects (
+	book INTEGER,
+	subject INTEGER,
+	FOREIGN KEY(book) REFERENCES books(id),
+	FOREIGN KEY(subject) REFERENCES subjects(id)
+);
