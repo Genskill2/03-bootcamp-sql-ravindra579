400ee5b0bd34a3d228c25b07d755665defd66932
ravindra <ravindrareddy1217@gmail.com>
2021-06-19 17:53:51 +0530

    final_done

--- query3.sql
+++ query3.sql
@@ -1,1 +1,1 @@
-select b.title from books b, subjects s, books_subjects k where k.book = b.id and k.subject = s.id and s.name = "Politics" or "Technology";
+select b.title from books b, subjects s, books_subjects k where k.book = b.id and k.subject = s.id and s.name IN ("Politics","Technology");
